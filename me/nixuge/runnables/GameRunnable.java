9406bd500e7705da3aa6f5388cdbbb98ea28f453
Nixuge <mail@nixuge.me>
2022-11-11 19:52:26 +0100

    Middle particles done

--- me/nixuge/runnables/GameRunnable.java
+++ me/nixuge/runnables/GameRunnable.java
@@ -13,6 +13,7 @@ import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 
 import me.nixuge.BlockSumo;
 import me.nixuge.McMap;
+import me.nixuge.runnables.particle.MiddleParticleRunnable;
 import net.minecraft.server.v1_8_R3.EnumParticle;
 import net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles;
 
@@ -23,9 +24,9 @@ public class GameRunnable extends BukkitRunnable {
     private int time = 0;
     private int last_bonus_spawn = 0;
 
-    private McMap map = BlockSumo.getInstance().getGameManager().getMcMap();
+    private BlockSumo plugin = BlockSumo.getInstance();
+    private McMap map = plugin.getGameManager().getMcMap();
     private Location loc = map.getCenter();
-    private Location particleLoc = loc.clone().add(0, +5, 0);
     private World world = map.getWorld();
     private Random rand = new Random();
 
@@ -33,27 +34,14 @@ public class GameRunnable extends BukkitRunnable {
     public void run() {
         // TODO:
         time++;
-        summonParticles();
+        
+        MiddleParticleRunnable run = new MiddleParticleRunnable(60);
+        run.runTaskTimer(plugin, 1, 1);
+        cancel();
+
     }
 
     private void summonParticles() {
-        // for (int i = 0; i < 20; i++) {
-        // //proper API only on 1.9+, to rework
-        // world.playEffect(loc, Effect.FLAME, 2, 1);
-        // }
-        float rdm = 0 + rand.nextFloat() * .47f; // perimeter of circle w .75 radius
-        float radius = 0.75f;
-        for (double y = rdm; y <= 3 + rdm; y += 0.025) {
-            double x = radius * Math.cos(y * 10);
-            double z = radius * Math.sin(y * 10);
-            //PacketPlayOutWorldParticles:
-            //particle, bool, x, y, z, x+-, y+-, z+-, idk, idk
-            PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.FLAME, true,
-                    (float) (particleLoc.getX() + x), (float) (particleLoc.getY() + y), (float) (particleLoc.getZ() + z), 0, 0, 0, 0, 0);
-            for (Player online : Bukkit.getOnlinePlayers()) {
-                ((CraftPlayer) online).getHandle().playerConnection.sendPacket(packet);
-            }
-        }
 
     }
 }
--- me/nixuge/runnables/particle/MiddleParticleRunnable.java
+++ me/nixuge/runnables/particle/MiddleParticleRunnable.java
@@ -1,7 +1,71 @@
 package me.nixuge.runnables.particle;
 
+import java.util.Random;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+
+import me.nixuge.BlockSumo;
+import net.minecraft.server.v1_8_R3.EnumParticle;
+import net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles;
+
 public class MiddleParticleRunnable extends BukkitRunnable {
-    
+
+    private final Random rand = new Random();
+    private final Location originalParticleLoc = BlockSumo.getInstance().getGameManager()
+            .getMcMap().getCenter();
+
+    private float y = 3;
+    private float offset = 0;
+    private Location particleLoc;
+    private int tick = 0;
+    private int maxTick = 0;
+
+    public MiddleParticleRunnable(int maxTick) {
+        this.maxTick = maxTick;
+    }
+
+
+    @Override
+    public void run() {
+        if (y > 2.1 + offset) {
+            offset = rand.nextFloat() * 1;
+            y = offset;
+            particleLoc = originalParticleLoc.clone().add(0, -y, 0);
+        }
+        summonCurrentParticle(0);
+        summonCurrentParticle(2f);
+        y += 0.05f;
+
+        if (tick >= maxTick) {
+            cancel();
+            return;
+        }
+        tick++;
+    }
+
+    private void summonCurrentParticle(float yAdder) {
+        float radius = 0.75f;
+        double x = radius * Math.cos((y + yAdder) * 5);
+        double z = radius * Math.sin((y + yAdder) * 5);
+
+        // PacketPlayOutWorldParticles:
+        // particle, bool, x, y, z, x+-, y+-, z+-, idk, idk
+        PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.FLAME, true,
+                (float) (particleLoc.getX() + x), (float) (particleLoc.getY() + y), (float) (particleLoc.getZ() + z), 0,
+                0, 0, 0, 1);
+
+        sendPacketAllPlayers(packet);
+    }
+
+    private void sendPacketAllPlayers(PacketPlayOutWorldParticles packet) {
+        for (Player online : Bukkit.getOnlinePlayers()) {
+            ((CraftPlayer) online).getHandle().playerConnection.sendPacket(packet);
+        }
+    }
+
 }
